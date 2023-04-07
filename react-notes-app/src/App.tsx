2b325a3d14d0bf526fcd57df242345cec37876f1
Achal1111 <achal.mehta@dripcapital.com>
2023-04-07 21:12:00 +0530

    Tag creation logic

--- react-notes-app/src/App.tsx
+++ react-notes-app/src/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React,{useMemo} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import {v4 as uuidV4} from 'uuid';
 
 export type Note = {
     id:string
@@ -19,12 +21,40 @@ export type Tag = {
     label:string;
 }
 
+export type RawNote = {
+    id:string;
+} & RawNoteData
+
+export type RawNoteData = {
+    title:string;
+    markdown:string;
+    tagIds: string[];
+}
+
 function App() {
+    const [notes,setNotes] = useLocalStorage<RawNote[]>('NOTES',[])
+    const [tags,setTags] = useLocalStorage<Tag[]>('TAGS',[])
+
+    const notesWithTags = useMemo(()=>{
+        return notes.map((notes)=>{
+            return {...notes,tags:tags.filter(tag=>notes.tagIds.includes(tag.id))}
+        })
+    },[notes,tags])
+
+    function onCreateNote({tags,...data}:NoteData) {
+        setNotes(prevNotes=>{
+            return [...prevNotes,{...data,id:uuidV4(),tagIds:tags.map(tag=>tag.id)}]
+        })
+    }
+
+    function addTag(tag:Tag){
+        setTags(prev=>[...prev,tag])
+    }
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>}></Route>
-        <Route path="/new" element={<NewNote />}></Route>
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />}></Route>
         <Route path="/:id">
           <Route index element={<h1>Show</h1>}></Route>
           <Route path="edit" element={<h1>Edit</h1>}></Route>
---
+++ react-notes-app/src/hooks/useLocalStorage.ts
@@ -0,0 +1,26 @@
+import {useState,useEffect} from 'react';
+
+export function useLocalStorage<T>(key:string,initialValue:T|(()=>T)) {
+    const [value,setValue] = useState<T>(()=>{
+        const jsonData = localStorage.getItem(key);
+        if(jsonData == null) {
+            if(typeof initialValue === 'function') {
+                return (initialValue as () => T)()
+
+            }
+            else{
+                return initialValue;
+            }
+        }
+        else {
+            return JSON.parse(jsonData)
+        }
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(key,JSON.stringify(value))
+    },[value,key])
+
+    return [value,setValue] as [T,typeof setValue]
+    
+}
