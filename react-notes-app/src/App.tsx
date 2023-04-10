863f6266cb224847abb947b7cfd4bace16ec6894
Achal1111 <achal.mehta@dripcapital.com>
2023-04-10 10:32:22 +0530

    Note Edit Page

--- react-notes-app/src/App.tsx
+++ react-notes-app/src/App.tsx
@@ -6,6 +6,9 @@ import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./hooks/useLocalStorage";
 import {v4 as uuidV4} from 'uuid';
 import { NoteList } from "./NoteList";
+import { NoteLayout } from "./NoteLayout";
+import { Note } from "./Note";
+import { EditNote } from "./EditNote";
 
 export type Note = {
     id:string
@@ -51,14 +54,28 @@ function App() {
     function addTag(tag:Tag){
         setTags(prev=>[...prev,tag])
     }
+
+    function onUpdateNote(id:string,{tags,...data}:NoteData){
+        setNotes(prevNotes=>{
+            return prevNotes.map(note=>{
+                if(note.id===id){
+                    return {...note,...data,tagIds:tags.map(tag=>tag.id)}
+                }
+                else {
+                    return note
+                }
+            })
+        })
+
+    }
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags}/>}></Route>
         <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />}></Route>
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>}></Route>
-          <Route path="edit" element={<h1>Edit</h1>}></Route>
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags}/>}>
+          <Route index element={<Note/>}></Route>
+          <Route path="edit" element={<EditNote onSubmit={onUpdateNote} onAddTag={addTag} availableTags={tags}/>}></Route>
         </Route>
         <Route path="*" element={<Navigate to="/" />}></Route>
       </Routes>
